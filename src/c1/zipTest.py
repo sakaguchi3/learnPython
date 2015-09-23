4e18841ca6eb01f3bb11b2c6bee2890e207f0f51
uqw <uqw@outlook.jp>
2015-09-24 02:52:56 +0900

    remove redundant parentheses

--- src/c1/zipTest.py
+++ src/c1/zipTest.py
@@ -1,6 +1,5 @@
-'''
-zip function test
-'''
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
 from builtins import int, str
 from typing import Tuple, List
 
@@ -8,16 +7,17 @@ from typing import Tuple, List
 def makeLstTuple() -> Tuple[List[int], List[str]]:
     lst1 = [i for i in range(0, 5)]
     lst2 = ['id_' + str(100 - i) for i in range(0, 10)]
-    return (lst1, lst2)
+    return lst1, lst2
 
 
-def myZip(tup: Tuple[List[int], List[str]]) -> List[str]:
-    lst = [_str + '_' + str(_int) for _int, _str in zip(t[0], t[1])]
+def ziptest(tup: Tuple[List[int], List[str]]) -> List[str]:
+    lst: List[str] = [_str + '_' + str(_int) for _int, _str in zip(t[0], t[1])]
     return lst
 
 
 if __name__ == '__main__':
     t = makeLstTuple()
-    r = myZip(t)
+    r = ziptest(t)
     pass
 
+pass
