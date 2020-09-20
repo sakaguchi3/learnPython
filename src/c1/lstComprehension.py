2e2db016f6d5a177dcb3a4dc167941bae9c9622d
uqw <uqw@outlook.jp>
2020-09-21 02:53:41 +0900

    list comprehension

--- src/c1/lstComprehension.py
+++ src/c1/lstComprehension.py
@@ -1,12 +1,16 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
 from builtins import int
-from typing import Dict, List
+from typing import List, Tuple
 
 
-def nothing():
+def tuple_f():
+    lst1: List[int] = [i for i in range(0, 10)]
+    lst2: List[int] = [i for i in range(0, 20) if i % 2 == 1]
+
+    lst3: List[Tuple[int, int]] = [(i, 100 - j) for i in range(0, 2) for j in range(0, 3)]
+    return None
     pass
 
 
-lst1: List[int] = [i for i in range(0, 10)]
-lst2: List[int] = [i for i in range(0, 20) if i % 2 == 1]
-
-nothing()
+pass
