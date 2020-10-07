fb2d4f5b3d8a838933a10d32b5e351b36a12ddc0
uqw <uqw@outlook.jp>
2020-10-08 02:42:44 +0900

    inequality

---
+++ src/c1/inequality.py
@@ -0,0 +1,17 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def hoge():
+    a = 10
+    b1 = 3 < a < 20
+    b2 = 11 < a < 20
+    b3 = 3 < a < 9
+    pass
+
+
+if __name__ == '__main__':
+    b = hoge()
+    pass
+
+pass
