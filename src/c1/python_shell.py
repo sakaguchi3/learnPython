8db4a1ec5967e290563a78751ed4a65c6f19fdde
uqw <uqw@outlook.jp>
2021-01-23 13:40:53 +0900

    python shell

---
+++ src/c1/python_shell.py
@@ -0,0 +1,22 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function, absolute_import, unicode_literals
+import os
+use1
+
+"""
+class exercise
+"""
+
+
+
+def exec():
+
+    pass
+
+
+if __name__ == '__main__':
+    exec()
+    pass
+pass
